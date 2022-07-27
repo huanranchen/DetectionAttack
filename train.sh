63b751b1410923ee35f4721ad0710d56533b61e7
celinechanny <1041276865@qq.com>
2022-07-27 16:33:11 +0800

    add detector

--- train.sh
+++ train.sh
@@ -6,21 +6,25 @@ patch_name=$config
 my_date=$(date '+%m-%d')
 #
 #
-echo "CUDA_VISIBLE_DEVICES=${device} python train.py \
+train_cmd="CUDA_VISIBLE_DEVICES=${device} python train.py \
 -cfg=${config}.yaml \
 -s=./results/coco/conf/${my_date}"
 
+echo $train_cmd
+eval $train_cmd
 
 # 测试模型自身+迁移的效果
 for i in {0..7}
 do
   config=inria$i
-  echo $config
-  echo "CUDA_VISIBLE_DEVICES=${device} python evaluate.py -i \
+#  echo $config
+  cmd="CUDA_VISIBLE_DEVICES=${device} python evaluate.py -i \
   -p ./results/inria/conf/${my_date}/patch/1000_${patch_name}.png \
   -cfg ./configs/${config}.yaml \
   -lp /home/chenziyan/work/BaseDetectionAttack/data/INRIAPerson/Test/labels \
   -dr /home/chenziyan/work/BaseDetectionAttack/data/INRIAPerson/Test/pos \
   -s /home/chenziyan/work/BaseDetectionAttack/data/inria/conf/${my_date} \
   -e 0 "
+  echo cmd
+  eval $cmd
 done
