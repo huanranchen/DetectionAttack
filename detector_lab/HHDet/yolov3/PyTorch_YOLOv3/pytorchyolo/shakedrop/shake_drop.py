c75f45169f5069bc5a96362a08579995f103439b
celinechanny <1041276865@qq.com>
2022-08-16 10:36:25 +0800

    add yolov3-shakedrop

--- detector_lab/HHDet/yolov3/PyTorch_YOLOv3/pytorchyolo/shakedrop/shake_drop.py
+++ detector_lab/HHDet/yolov3/PyTorch_YOLOv3/pytorchyolo/shakedrop/shake_drop.py
@@ -43,31 +43,3 @@ class ShakeDrop(torch.autograd.Function):
             return beta * grad_output, None, None, None
         else:
             return grad_output, None, None, None
-
-
-# class ShakeDrop(torch.autograd.Function):
-#
-#     @staticmethod
-#     def forward(ctx, x, training:bool =False, p_drop: float =0.5, alpha_range: list =[-1, 1]):
-#         if training:
-#             gate = torch.cuda.FloatTensor([0]).bernoulli_(1 - p_drop)
-#             ctx.save_for_backward(gate)
-#             if gate.item() == 0:
-#                 alpha = torch.cuda.FloatTensor(x.size(0)).uniform_(*alpha_range)
-#                 alpha = alpha.view(alpha.size(0), 1, 1, 1).expand_as(x)
-#                 return alpha * x
-#             else:
-#                 return x
-#         else:
-#             return (1 - p_drop) * x
-#
-#     @staticmethod
-#     def backward(ctx, grad_output):
-#         gate = ctx.saved_tensors[0]
-#         if gate.item() == 0:
-#             beta = torch.cuda.FloatTensor(grad_output.size(0)).uniform_(0, 1)
-#             beta = beta.view(beta.size(0), 1, 1, 1).expand_as(grad_output)
-#             beta = Variable(beta)
-#             return beta * grad_output, None, None, None
-#         else:
-#             return grad_output, None, None, None
