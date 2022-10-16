6b43452242b4a499ca83a802ca0175c1fd720e26
Andrew Chen <1062446749@qq.com>
2022-10-16 16:02:25 +0800

    chr: add some optimizer

--- scripts/dict.py
+++ scripts/dict.py
@@ -10,7 +10,9 @@ scheduler_factory = {
 import torch
 
 optim_factory = {
-    'optim': lambda p_obj, lr: torch.optim.Adam([p_obj], lr=lr, amsgrad=True), # default
+    'optim': lambda p_obj, lr: torch.optim.Adam([p_obj], lr=lr, amsgrad=True),  # default
     'optim-adam': lambda p_obj, lr: torch.optim.Adam([p_obj], lr=lr, amsgrad=True),
-    'optim-sgd': lambda p_obj, lr: torch.optim.SGD([p_obj], lr=lr)
+    'optim-sgd': lambda p_obj, lr: torch.optim.SGD([p_obj], lr=lr * 100),
+    'optim-nesterov': lambda p_obj, lr: torch.optim.SGD([p_obj], lr=lr * 100, nesterov=True, momentum=0.9),
+    'optim-rmsprop': lambda p_obj, lr: torch.optim.RMSprop([p_obj], lr=lr)
 }
