8897faa515927cbfa3a93770e0f4db7dc1eefef6
celinechanny <1041276865@qq.com>
2022-10-16 22:08:41 +0800

    fix cfg -czy

--- scripts/dict.py
+++ scripts/dict.py
@@ -13,7 +13,7 @@ scheduler_factory = {
 optim_factory = {
     'optim': lambda p_obj, lr: torch.optim.Adam([p_obj], lr=lr, amsgrad=True), # default
     'optim-adam': lambda p_obj, lr: torch.optim.Adam([p_obj], lr=lr, amsgrad=True),
-    'optim-sgd': lambda p_obj, lr: torch.optim.SGD([p_obj], lr=lr),
+    'optim-sgd': lambda p_obj, lr: torch.optim.SGD([p_obj], lr=lr * 100),
     'optim-nesterov': lambda p_obj, lr: torch.optim.SGD([p_obj], lr=lr * 100, nesterov=True, momentum=0.9),
     'optim-rmsprop': lambda p_obj, lr: torch.optim.RMSprop([p_obj], lr=lr * 100)
 }
