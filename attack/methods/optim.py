b23e3402007b5f79717f7dfd91b824e6627bf108
Andrew Chen <1062446749@qq.com>
2022-10-17 10:06:11 +0800

    chr: add notes

--- attack/methods/optim.py
+++ attack/methods/optim.py
@@ -1,19 +1,22 @@
 import numpy as np
 import torch
 from .base import BaseAttacker
+from torch.optim import Optimizer
 
 
 class OptimAttacker(BaseAttacker):
     def __init__(self, device, cfg, loss_func, detector_attacker, norm='L_infty'):
         super().__init__(loss_func, norm, cfg, device, detector_attacker)
 
-    def set_optimizer(self, optimizer):
+    def set_optimizer(self, optimizer: Optimizer):
         self.optimizer = optimizer
         if self.detector_attacker.vlogger is not None:
             self.detector_attacker.vlogger.optimizer = optimizer
 
     def patch_update(self, **kwargs):
         self.optimizer.step()
+        # grad = self.optimizer.param_groups[0]['params'][0].grad
+        # print(torch.mean(torch.abs(grad)))
         self.patch_obj.clamp_(p_min=self.min_epsilon, p_max=self.max_epsilon)
 
     def attack_loss(self, confs):
