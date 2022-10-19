1e1fc21576df226a5a6285b66c33b693cffb364d
celinechanny <1041276865@qq.com>
2022-10-19 12:17:11 +0800

    fix shakedrop -czy

--- scripts/train.sh
+++ scripts/train.sh
@@ -5,8 +5,15 @@ model=$3
 name=$4
 
 mkdir ./results/exp4/$target/$model
+
 CUDA_VISIBLE_DEVICES=$cuda nohup python train_optim.py \
 -cfg=$target/$model.yaml \
 -s=./results/exp4/$target/$model/ \
 -n=$model-$name \
 >./results/exp4/$target/$model/$model-$name.log 2>&1 &
+
+nohup bash test.sh $cuda \
+exp4/$target/$model/$model-$name \
+exp4/$target/$model/ \
+"eval/coco80 eval/coco91" \
+"Test" &
